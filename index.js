8dca3ab67ef09459d83addc7e8a154c76a771062
Bibin Antony <40894472+Bibin-Antony@users.noreply.github.com>
2021-09-22 02:30:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ const outputBox = document.querySelector("#output-box")
 calculateBtn.addEventListener("click", profitlosscalc);
 
 function profitlosscalc(){
+    outputBox.style.display = "none";
     var InitialPrice = Number(firstValue.value);
     var Quantity = Number(secondValue.value);
     var CurrentPrice = Number(thirdValue.value);
     var stockDiff = CurrentPrice - InitialPrice;
     if(InitialPrice > CurrentPrice){
+        outputBox.style.display = "block";
         var stockDiff = (InitialPrice - CurrentPrice)*Quantity;
         var finalValue = (stockDiff/InitialPrice)*100;
-    }else{
+        outputBox.innerText = `Hey, the profit is ${stockDiff} stock and the percent is ${finalValue}%`;
+    }else if(CurrentPrice > InitialPrice){
+        outputBox.style.display = "block";
         var stockDiff = (CurrentPrice - InitialPrice)*Quantity;
         console.log(stockDiff, InitialPrice, CurrentPrice);
         var finalValue = (stockDiff/InitialPrice)*100;
+        console.log(typeof finalValue);
         outputBox.innerText = `Hey, the profit is ${stockDiff} stock and the percent is ${finalValue}%`;//templete litreals
+    }else{
+        console.log(CurrentPrice);
+        outputBox.style.display = "block";
+        outputBox.innerText = `NO PAIN NO GAIN (Atleast Enter values)`;
     }
 
    
